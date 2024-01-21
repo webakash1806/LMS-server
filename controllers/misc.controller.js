fee6bc6d1e2526538990461a85855a98f0117159
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-01-21 10:08:21 +0530

    Contact Us functionality added

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -3,6 +3,36 @@ import User from "../models/user.models.js";
 
 // Importing the custom error handling utility
 import AppError from "../utils/error.utils.js";
+import sendEmail from '../utils/sendEmail.js'
+
+
+
+export const contactUs = async (req, res, next) => {
+    const { name, email, message } = req.body
+
+    if (!name || !email || !message) {
+        return next(new AppError('All fields are required', 400))
+    }
+    try {
+        const subject = 'Contact Us Form';
+        const textMessage = `${name} - ${email} <br /> ${message}`;
+
+        // Await the send email
+        await sendEmail(process.env.CONTACT_US_EMAIL, subject, textMessage);
+
+        // Sending the success response
+        res.status(200).json({
+            success: true,
+            message: 'Your request has been submitted successfully',
+        });
+    } catch (error) {
+        // Handling errors by passing them to the next middleware (error handling middleware)
+        return next(new AppError(error.message, 400));
+    }
+
+
+}
+
 
 // Controller function to retrieve and send user statistics
 export const userStats = async (req, res, next) => {
--- routes/Misc.routes.js
+++ routes/Misc.routes.js
@@ -4,8 +4,8 @@ import { Router } from "express";
 // Importing middleware functions from the 'auth.middleware.js' file
 import { isLoggedIn, authorizedUser } from "../middlewares/auth.middleware.js";
 
-// Importing the 'userStats' controller function from the 'misc.controller.js' file
-import { userStats } from "../controllers/misc.controller.js";
+// Importing the 'userStats' and 'contactUs controller function from the 'misc.controller.js' file
+import { contactUs, userStats } from "../controllers/misc.controller.js";
 
 // Creating an instance of the 'Router' class to define routes
 const router = Router();
@@ -14,6 +14,9 @@ const router = Router();
 // Middleware functions 'isLoggedIn' and 'authorizedUser' are used to check authentication and user roles
 // The 'userStats' controller function is responsible for handling the actual logic of the route
 router.route('/admin/stats').get(isLoggedIn, authorizedUser('ADMIN'), userStats);
+
+// Defining a route for handling POST requests to '/contact'
+router.route('/contact').post(contactUs)
 
 // Exporting the router instance to be used in other parts of the application
 export default router;
